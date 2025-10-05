00286b6e00730aa72b284e687429454112fa2ace
Pablo Czurylo <pabloczurylo10@gmail.com>
2025-10-05 14:54:25 -0300

    Acomode iconos para vista en pantallas grandes

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,57 +3,70 @@ import { Navbar, Nav, Container } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import { FaUser, FaSearch, FaShoppingCart } from 'react-icons/fa'; 
 
+// Componente para los iconos de utilidad (Login, Búsqueda, Carrito)
+// Esto evita duplicar el JSX de los iconos
+const UtilityIcons = () => (
+    <div className="d-flex align-items-center">
+        <NavLink to="/search" className="text-dark fs-5 p-2 nav-link-custom"><FaSearch /></NavLink>
+        <NavLink to="/cart" className="text-dark fs-5 p-2 position-relative nav-link-custom">
+            <FaShoppingCart />
+            <span style={{ position: 'absolute', top: '0px', right: '-2px', fontSize: '0.6em', backgroundColor: 'red', color: 'white', borderRadius: '50%', padding: '2px 5px', lineHeight: '1', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>0</span>
+        </NavLink>
+        <NavLink to="/login" className="text-dark fs-5 p-2 nav-link-custom"><FaUser /></NavLink>
+    </div>
+);
+
 const NavbarComponent = () => {
-  return (
-    <Navbar bg="light" expand="lg" className="py-3 border-bottom" fixed="top"> 
-      <Container className="d-flex justify-content-between align-items-center"> 
-        {/* Usamos d-flex y justify-content-between para distribuir los elementos extremos */}
-        
-        {/* 1. Logo (PH LOX) - Izquierda, alineado */}
-        <Navbar.Brand 
-            as={NavLink} 
-            to="/" 
-            className="fw-bolder fs-5 text-dark" 
-        >
-            PH LOX
-        </Navbar.Brand>
-        
-        {/* 2. Íconos de Utilidad y Toggle (Derecha, siempre visibles) */}
-        {/* Este div debe ir antes del Navbar.Collapse para que los íconos se mantengan a la derecha del logo en desktop */}
-        <div className="d-flex align-items-center ms-auto">
-            
-            {/* Íconos de Utilidad (Login, Search, Cart) - HORIZONTAL Y FUERA DEL MENÚ */}
-            <div className="d-flex align-items-center"> 
+    return (
+        <Navbar bg="light" expand="lg" className="py-3 border-bottom" fixed="top"> 
+            <Container>
                 
-                <NavLink to="/search" className="text-dark fs-5 p-2 nav-link-custom"><FaSearch /></NavLink>
-                <NavLink to="/cart" className="text-dark fs-5 p-2 position-relative nav-link-custom">
-                    <FaShoppingCart />
-                    <span style={{ position: 'absolute', top: '0px', right: '-2px', fontSize: '0.6em', backgroundColor: 'red', color: 'white', borderRadius: '50%', padding: '2px 5px', lineHeight: '1', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>0</span>
-                </NavLink>
-                <NavLink to="/login" className="text-dark fs-5 p-2 nav-link-custom"><FaUser /></NavLink>
-            </div>
+                {/* 1. Logo (PH LOX) */}
+                <Navbar.Brand 
+                    as={NavLink} 
+                    to="/" 
+                    className="fw-bolder fs-5 text-dark me-5" 
+                >
+                    PH LOX
+                </Navbar.Brand>
 
-            {/* Toggle (Botón Hamburguesa) - Solo visible en móvil, a la derecha de los íconos */}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-3 d-lg-none" /> 
-            
-        </div>
-        
-        {/* 3. Links Principales - Se colapsan/ocultan en móvil (lg) */}
-        {/* Debe estar después de los íconos para que los íconos se prioricen a la derecha */}
-        {/* Usamos una clase personalizada para arreglar el despliegue en móvil */}
-        <Navbar.Collapse id="basic-navbar-nav" className="custom-collapse-mobile">
-          <Nav className="mx-auto mb-2 mb-lg-0"> 
-            <Nav.Link as={NavLink} to="/" className="text-dark px-3 fw-medium nav-link-custom">Inicio</Nav.Link>
-            {/* ... el resto de tus Nav.Link ... */}
-            <Nav.Link as={NavLink} to="/shop" className="text-dark px-3 fw-medium nav-link-custom">Tienda</Nav.Link>
-            <Nav.Link as={NavLink} to="/about" className="text-dark px-3 fw-medium nav-link-custom">Acerca de</Nav.Link>
-            <Nav.Link as={NavLink} to="/contact" className="text-dark px-3 fw-medium nav-link-custom">Contáctanos</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+                
+                {/* 2. Íconos de Utilidad y Toggle para MÓVIL (Versión Pequeña) */}
+                {/* Este bloque es el que mantiene los íconos y el toggle visibles en móvil (d-flex) */}
+                <div className="d-flex align-items-center d-lg-none">
+                    
+                    <UtilityIcons /> {/* Íconos siempre visibles en móvil */}
+                    
+                    {/* Toggle (Botón Hamburguesa) - Oculto en desktop (d-lg-none) */}
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" className="ms-3 d-lg-none" /> 
+                    
+                </div>
+                
+                
+                {/* 3. Links Principales y Íconos de Utilidad para DESKTOP (Versión Grande) */}
+                {/* El Navbar.Collapse se usa para el layout de Desktop */}
+                <Navbar.Collapse id="basic-navbar-nav">
+                    
+                    {/* Links Principales - Usamos 'me-auto' para empujar los iconos a la derecha en DESKTOP */}
+                    <Nav className="me-auto mb-2 mb-lg-0"> 
+                        <Nav.Link as={NavLink} to="/" className="text-dark px-3 fw-medium nav-link-custom">Inicio</Nav.Link>
+                        <Nav.Link as={NavLink} to="/shop" className="text-dark px-3 fw-medium nav-link-custom">Tienda</Nav.Link>
+                        <Nav.Link as={NavLink} to="/about" className="text-dark px-3 fw-medium nav-link-custom">Sobre Nosotros</Nav.Link>
+                        <Nav.Link as={NavLink} to="/blog" className="text-dark px-3 fw-medium nav-link-custom">Blog</Nav.Link>
+                        <Nav.Link as={NavLink} to="/contact" className="text-dark px-3 fw-medium nav-link-custom">Contáctanos</Nav.Link>
+                    </Nav>
 
-      </Container>
-    </Navbar>
-  );
+                    {/* Íconos de Utilidad para DESKTOP - Oculto en móvil (d-none) */}
+                    {/* ¡El Nav que contiene los íconos de Login/Search/Cart en desktop está aquí! */}
+                    <Nav className="d-none d-lg-flex align-items-center">
+                        <UtilityIcons />
+                    </Nav>
+
+                </Navbar.Collapse>
+
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavbarComponent;
